344a7804d442293bf07435872aaa922c7089f73c
satyabrata-21 <satyabratabeher826@gmail.com>
2025-11-18 22:36:00 +0530

    Added 3 files

---
+++ Input.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Enter your name: "
+
+read username
+
+echo "Welcome $username"
---
+++ variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+a="satyabrata"
+b="kunal"
+
+echo "My name is $a"
+
+echo "My nick name is $b"
